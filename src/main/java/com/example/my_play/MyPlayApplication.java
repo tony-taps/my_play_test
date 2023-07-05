4aed08f2f95c5728c6fbd545ed1ec2c7494d4bbd
TAPIWA T. Chigumira <tchigumira@bancabc.co.zw>
2023-07-05 18:58:17 +0200

    first commit

---
+++ src/main/java/com/example/my_play/MyPlayApplication.java
@@ -0,0 +1,13 @@
+package com.example.my_play;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPlayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPlayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/my_play/MyPlayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.my_play;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPlayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
